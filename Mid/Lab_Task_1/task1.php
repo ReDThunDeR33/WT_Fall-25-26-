a34b534f2725f17504cd71027a7ec3234806ddfd
Arindom-Lane <arindom793@gmail.com>
2025-11-03 12:41:29 +0600

    task 1 submmited

---
+++ Mid/Lab_Task_1/task1.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Task 1</title>
+</head>
+
+<table style="margin :550px ;background-color: cyan ">
+    <tr>
+        <td>
+
+<body style = "background-color:pink">
+
+
+    <h2 style ="color : red;">Student Registration Information </h2>
+    <hr color="red" >
+
+First name
+<br>
+<input type="text"size="50"  >
+
+</br>
+<br>
+Last name
+<br>
+<input type="text"size="50" >
+</br><br>
+Student ID
+<br>
+<input type="text"size="50" >
+</br><br>
+Program/Major
+<br>
+<select name="program">
+  <option value="Program">Select Program</option>
+  <option value="BBA">BBA</option>
+  <option value="CSE">CSE</option>
+  <option value="EEE">EEE</option>
+  <option value="LLB">LLB</option>
+</br>
+</select><br>
+<br>
+Department
+<br>
+<select name="department">
+    <option value="Select Department">Select Department</option>  
+  <option value="Business">Business</option>
+  <option value="Engineering">Engineering</option>
+  <option value="Law">Law</option>
+</select><br>
+<br>
+Phone number
+<br>
+<input type="text" maxlength="11" size="50> >
+<br><br>
+University Email
+<br>
+<input type="text" size="50" >
+<br><br>
+Create Password (minimum 8 characters)
+<br>
+<input type="password" size="50">
+<br><br>
+Confirm Password
+<br>
+<input type="password" size="50">
+<br><br>
+Semester Selection
+<br>
+<input type ="radio"> Summer 2024
+
+<input type ="radio"> Fall 2024
+
+
+<input type ="radio"> Spring 2024
+
+<input type="radio"> Other/Special Term
+<br>
+
+
+<br>
+Request Credit Load(Max 15)
+<br>
+<input type ="number" placeholder="e.g, 9 or 12"size="50">
+<br>
+<input type="checkbox"> I require acdemic advising before final registration
+<br>
+
+<h2 style ="color : red;">Course Selection </h2>
+    <hr color="red" >
+    <br>
+    <input type="checkbox"> MATH 1201 (Calculus 1) <input type="checkbox"> CS 2105 (Data Structures) <input type="checkbox"> ECON 1001 (Microeconomics)
+    <br>
+    <input type="checkbox"> PHY 1102 (Physics Lab)
+<br>
+<br>
+Comments/Special Requests
+<br>
+<input type ="text" style="width :380px;height: 100px;">
+<br><br>
+<input type ="submit">
+
+</td>
+</tr>
+</table>
+
+</body>
+    
+    </html>
