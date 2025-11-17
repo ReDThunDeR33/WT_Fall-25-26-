ed97bb8055acbbac9219f8a6dd34d194ab98756e
Ayan <169395060+ReDThunDeR33@users.noreply.github.com>
2025-11-17 11:11:25 +0600

    2

--- Mid/Lab_Task_Final/Lab_Task_Final.php
+++ Mid/Lab_Task_Final/Lab_Task_Final.php
@@ -6,4 +6,21 @@
 
 
 </style>
+
+
+<body style="background-color:rgba(242, 188, 155, 0.44);">
+
+<center>
+<form>
+<h1>Participant Registraation</h1>
+<br></br>
+</form>
+
+
+
+
+</center>
+
+
+</body>
 </html>
