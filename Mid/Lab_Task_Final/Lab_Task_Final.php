146fb5094491cf13091e47481202dc4ee1f8a0cf
Ayan <169395060+ReDThunDeR33@users.noreply.github.com>
2025-11-17 11:23:03 +0600

    4

--- Mid/Lab_Task_Final/Lab_Task_Final.php
+++ Mid/Lab_Task_Final/Lab_Task_Final.php
@@ -3,7 +3,17 @@
 <title>Lab Task Final</title>
 <style>
 
-
+.f{
+    border: groove ;
+    background-color:rgba(251, 251, 251, 1);
+    margin: 20px 480px 0px 480px;
+    border-radius: 12px;
+    padding: 10px ;
+    text-align: left;
+}
+.gc_collect_cycles{
+    
+}
 
 </style>
 
@@ -11,9 +21,16 @@
 <body style="background-color:rgba(242, 188, 155, 0.44);">
 
 <center>
-<form>
-<h1>Participant Registraation</h1>
-<br></br>
+<form class="f">
+<h1 style="text-align: center;">Participant Registraation</h1>
+<br>
+<div class="g">
+Full Name:
+<br>
+<input type="text" style="width:480px;">
+
+
+</div>
 </form>
 
 
