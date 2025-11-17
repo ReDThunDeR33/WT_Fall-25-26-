556218ae286e669cb59883214041b6b9e6663f6b
Ayan <169395060+ReDThunDeR33@users.noreply.github.com>
2025-11-17 11:46:47 +0600

    44

--- Mid/Lab_Task_Final/Lab_Task_Final.php
+++ Mid/Lab_Task_Final/Lab_Task_Final.php
@@ -11,8 +11,13 @@
     padding: 10px ;
     text-align: left;
 }
-.gc_collect_cycles{
-    
+.a{
+    border: groove ;
+    background-color:rgba(251, 251, 251, 1);
+    margin: 20px 480px 0px 480px;
+    border-radius: 12px;
+    padding: 10px ;
+    text-align: left;
 }
 
 </style>
@@ -21,51 +26,80 @@
 <body style="background-color:rgba(242, 188, 155, 0.44);">
 
 <center>
-<form class="f">
-<h1 style="text-align: center;">Participant Registraation</h1>
+<form class="f" onsubmit="return form1()">
+<h2 style="text-align: center;">Participant Registraation</h2>
 <br>
 <div class="g">
 Full Name:
 <br>
-<input type="text" style="width:480px;">
+<input type="text" style="width:480px;" id="fname">
 <br></br>
 Email:
 <br>
-<input type ="email" style="width:480px;">
+<input type ="email" style="width:480px;"id="email">
 <br></br>
 Phone Number
 <br>
-<input type="tel" style="width:480px;">
+<input type="tel" style="width:480px;" id="phone">
 <br></br>
 Password:
 <br>
-<input type="password" style="width:480px;">
+<input type="password" style="width:480px;" id="pass">
 <br></br>
 Confirm Password:
 <br>
-<input type="password" style="width:480px;">
+<input type="password" style="width:480px;" id="confirm_pass">
 <br></br>
-<input type="submit" style="width:100px;" value="Register">
+<input type="submit" style="width:100px; color:white; background-color:blue;" value="Register">
 </div>
-<div class="output">
+<div id="output">
 </div>
-<div class="error">
+<div id="error">
 </div>
 
 
 </form>
 
+<br></br>
+
 
 
 <form class="a">
+<h2 style="text-align: center;">Activity Selection</h2>
+<br>
 
+<div class="activity">
+Activity Name:
+<br>
+<input type="text" style="width:480px;">
+<br></br>
+<input type="submit" style="width:100px; color:white; background-color:blue;" value="Add Activity">
 
-
+</div>
+<br></br>
+<div class="output2">
+</div>
 
 </form>
 
 
 </center>
+
+
+<script>
+function form1(){
+    var fname=document.getElementById("fname").value;
+    var email=document.getElementById("email").value;
+    var phone=document.getElementById("phone").value;
+    var pass=document.getElementById("pass").value;
+    var confirm_pass=document.getElementById("confirm_pass").value;
+
+    var error_msg=document.getElementById("error");
+    var output_msg=document.getElementById("output");
+
+    }
+
+</script>
 
 
 </body>
